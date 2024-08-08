602a3fe1c777dde07bce9fef6dde1216c86b6b33
Jacob Pantuso <jacobpantuso@outlook.com>
2024-08-08 11:55:09 -0400

    Added under consutrction page and adjusted styles

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 /* Countdown Timer */
 var countDownDate = new Date("Sep 26, 2025 15:00:00").getTime();
-console.log('hello')
 // Update the count down every 1 second
 var x = setInterval(function() {
 
@@ -25,3 +24,30 @@ var x = setInterval(function() {
     document.getElementById("demo").innerHTML = "EXPIRED";
   }
 }, 1000);
+
+window.addEventListener('scroll', function() {
+  var navbar = document.getElementById('navbar');
+  if (window.scrollY > 50) { // Adjust the scroll value as needed
+      navbar.classList.add('nav-scrolled');
+  } else {
+      navbar.classList.remove('nav-scrolled');
+  }
+});
+
+window.onload = function() {
+  var isAuth = localStorage.getItem('isAuth');
+  if (isAuth) {
+    document.getElementById('under-construction').style.display = 'none';
+    document.getElementById('main').style.display = 'flex';
+  } else {
+    document.getElementById('under-construction').style.display = 'flex';
+    document.getElementById('main').style.display = 'none';
+  }
+}
+
+document.getElementById('login').addEventListener('click', function() {
+  if(document.getElementById('password').value === 'FinleyNoahCaitlin1999') {
+    localStorage.setItem('isAuth', true);
+    window.location.reload();
+  }
+});
